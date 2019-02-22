6dbea248a5dfad65960ed4ee183b5c08823f83a1
presentheo <dbcjdgk@gmail.com>
2019-02-22 15:06:35 +0900

    불필요한 코드 삭제, 설정 파일 수정

--- mise-frontend/src/components/Menu.js
+++ mise-frontend/src/components/Menu.js
@@ -7,7 +7,6 @@ const MenuCover = styled.div`
   position: fixed;
   width: 100%;
   height: 100vh;
-  /* background-color: rgba(0,0,0,0.4); */
   z-index: 5;
   display: ${props => props.open ? 'block' : 'none'};
 `
